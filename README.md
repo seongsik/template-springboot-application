8cd256f5678fa18cc6f2bbd3abbabd26aa8f38d5
sik <soung371@gmail.com>
2022-11-08 13:46:20 +0900

    Swagger Config

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # template-springboot-application
+
+=============
+
+## 참고문헌
+* 전문가를 위한 Spring5 개정판, 길벗출판사
+* 스프링 부트와 AWS로 혼자 구현하는 웹 서비스
+
+## Environments
+* Framework : Spring Boot 2.7.5
+* SDK : Azul zulu version 17.0.4
+* Gradle : 7.5.1
+
+
+=============
+### Swagger
+* Spring-Fox : Spring Boot 2.6 이하
+* Spring-Doc : Spring boot 2.6 이상 
+
+#### Dependencies
+```groovy
+implementation 'org.springdoc:springdoc-openapi-ui:1.6.6'
+```
+
+#### Config
+* [SwaggerConfig.java](src/main/java/com/sik/template/config/SwaggerConfig.java)
+* [IndexController.java](src/main/java/com/sik/template/biz/IndexController.java)
+
+
+
+
---
+++ src/main/java/com/sik/template/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.sik.template.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Template Springboot API")
+                        .description("Template Springboot API Description.")
+                        .version("v0.0.1")
+                );
+    }
+}
