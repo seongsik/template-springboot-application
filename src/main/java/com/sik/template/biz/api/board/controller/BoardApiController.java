149f55b97ee2f590ebd7b3ce48fba170c4409714
sik <soung371@gmail.com>
2022-11-10 17:19:13 +0900

    swagger 설명 추가

--- src/main/java/com/sik/template/biz/api/board/controller/BoardApiController.java
+++ src/main/java/com/sik/template/biz/api/board/controller/BoardApiController.java
@@ -4,8 +4,10 @@ import com.sik.template.biz.api.base.response.RestApiResponse;
 import com.sik.template.biz.api.board.dto.BoardDTO;
 import com.sik.template.biz.api.board.service.BoardApiService;
 import com.sik.template.biz.api.board.vo.BoardVO;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +18,11 @@ import java.util.List;
 public class BoardApiController {
     BoardApiService boardApiService;
 
-    @GetMapping("/")
+    @Operation(
+            summary = "게시글 목록"
+            , description = "게시글 목록을 조회한다."
+    )
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public RestApiResponse<BoardVO, List<BoardDTO>> findAllBoard(Pageable pageable, @ModelAttribute BoardVO vo) {
         RestApiResponse<BoardVO, List<BoardDTO>> res = new RestApiResponse<>();
 
@@ -27,7 +33,11 @@ public class BoardApiController {
         return res;
     }
 
-    @GetMapping("/{searchId}")
+    @Operation(
+            summary = "게시글 조회"
+            , description = "단일 게시글 정보를 조회한다."
+    )
+    @GetMapping(value = "/{searchId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public RestApiResponse<BoardVO, BoardDTO> findBoard(@PathVariable Long searchId) {
         RestApiResponse<BoardVO, BoardDTO> res = new RestApiResponse<>();
 
